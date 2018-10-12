be2d5f4d250e3c8132eabd07df88edae886eaa05
wildmolasses <changes@gmail.com>
2018-10-12 12:01:21 -0400

    Remove fields, add filtered address route

--- api/create-table.js
+++ api/create-table.js
@@ -2,14 +2,21 @@ const AppDAO         = require('./dao')
 const connection = new AppDAO('./database.sqlite3')
 
 
+// const sql = `
+//     CREATE TABLE IF NOT EXISTS block_address (
+//       block_number INTEGER NOT NULL,
+//       tx_index INTEGER,
+//       trace_id INTEGER,
+//       address VARCHAR(42) NOT NULL,
+//       reason VARCHAR(255) NOT NULL,
+//       proof VARCHAR(36)
+//       )`
+
 const sql = `
     CREATE TABLE IF NOT EXISTS block_address (
       block_number INTEGER NOT NULL,
       tx_index INTEGER,
-      trace_id INTEGER,
-      address VARCHAR(42) NOT NULL,
-      reason VARCHAR(255) NOT NULL,
-      proof VARCHAR(36)
+      address VARCHAR(42) NOT NULL
       )`
 
 connection.run(sql);
